c728b8624f1e18c337b69d967106de914bee8e91
gabrielamoniz <monizdea@sheridancollege.ca>
2023-01-25 12:34:42 -0500

    version 1 students details

---
+++ src/printstudentdetails/PrintStudentDetails.java
@@ -0,0 +1,35 @@
+
+
+package printstudentdetails;
+
+
+/**Array of objects - java example: print student details
+ * Project - share to github : collaboration tool, tracking changes keep history - version control software
+ * char[] reverse = new char[word.length()]; - reverse of word 
+ * print 3 students details are name, age store in array then print array of objects
+ * @author Gabriela Moniz
+ */
+public class PrintStudentDetails {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        //used for user interaction printing, input
+        
+        Student [] list = new Student[3]; //array of objects declaration
+        //create students 3 - object 
+        Student s1 = new Student();
+        s1.setName("Peter");
+        s1.setAge(20);
+        list[0] = s1;
+        System.out.println(list[0].getName() + list[0].getAge());
+        
+       //for loop 
+    //Student s2 = new Student();
+        
+        
+       //Student s3 = new Student();
+    }
+
+}
---
+++ src/printstudentdetails/Student.java
@@ -0,0 +1,44 @@
+
+
+package printstudentdetails;
+
+/**model class
+ *define an entity Student attributes name, id, age, functions / methods etc
+ * encapsulation principle - object oriented design principals
+ * local rep in laptop: 1 - initialize version 2 - .gitconfig 3 - .gitignore
+ * netbeans code - working tree code - to local rep - commit;
+ * @author Gabriela Moniz
+ */
+public class Student {
+    private String name;
+    private int age;
+
+    /**
+     * @return the name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * @param name the name to set
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * @return the age
+     */
+    public int getAge() {
+        return age;
+    }
+
+    /**
+     * @param age the age to set
+     */
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
+
